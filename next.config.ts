4cefc0da7648fecbce372bdd74846e2ea5bd8001
Mark Balajadia <markb.freelance@gmail.com>
2026-03-20 14:13:41 +0800

    additional config update

--- next.config.ts
+++ next.config.ts
@@ -8,7 +8,7 @@ const nextConfig: NextConfig = {
     formats: ['image/avif', 'image/webp'],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920],
     minimumCacheTTL: 60 * 60 * 24 * 30, // 30 days
-    qualities: [75],
+    qualities: [75, 100],
   },
   // Reduce JS bundle size
   compiler: {
