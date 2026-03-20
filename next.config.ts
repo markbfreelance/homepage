cf3c7177c45121f6475a5aa4fd3ceaf9ba8b901a
Mark Balajadia <markb.freelance@gmail.com>
2026-03-20 13:56:42 +0800

    additional config update

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  experimental: {
+    optimizeCss: true,
+  },
+  images: {
+    formats: ['image/avif', 'image/webp'],
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920],
+    minimumCacheTTL: 60 * 60 * 24 * 30, // 30 days
+  },
+}
 
-export default nextConfig;
+export default nextConfig
